12a6edcdae8f70f638fd94bc38f5de9a4fa042ea
hrbcksq <hrbcksq@gmail.com>
2016-06-25 22:13:36 +0300

    Templates lesson

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
         selector: 'courses',
-        template: '<h3>Courses Component</h3>'
+        template: `
+                <h3>Courses Component</h3>
+                {{title}}
+                <ul>
+                        <li *ngFor='#course of courses'>
+                                {{course}}
+                        </li>
+                </ul>
+                `
 })
-export class CoursesComponent { }
+export class CoursesComponent {
+        title = "The title of courses page";
+        courses = ['Course1', 'Course2', 'Course3'];
+}
