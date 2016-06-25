ad80860bf86b6b6b48597678aef2591d672a1850
hrbcksq <hrbcksq@gmail.com>
2016-06-25 22:22:54 +0300

    11.Services

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -14,5 +14,5 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
         title = "The title of courses page";
-        courses = ['Course1', 'Course2', 'Course3'];
+        courses = [];
 }
