ca5fce6f14c2635b5046196aeb0fa88a432d4859
hrbcksq <hrbcksq@gmail.com>
2016-06-25 21:43:20 +0300

    Courses branch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { CoursesComponent } from './courses.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h2> My first Angualr 2 App </h2>'
+    template: `
+        <h2> My first Angualr 2 App </h2>
+        <courses></courses>`,
+    directives: [CoursesComponent]
 })
-export class AppComponent{ }
+export class AppComponent { }
