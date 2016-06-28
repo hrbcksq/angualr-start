0b6e9a06e71eb3ce0ab6f3b0343f64e8a237ac09
hrbcksq <hrbcksq@gmail.com>
2016-06-28 20:14:32 +0300

    Style binding example

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,14 @@ import { CoursesComponent } from './courses.component'
 import { AuthorsComponent } from './authors.component'
 
 // Property binding works only one way
+// Class binding is the way to bind property value to class
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>{{ title }}</h1>
         <img src='{{image}}' width='{{size}}' height='{{size}}'>
-        <input class='btn btn-primary' type='button' [class.active]='isActive' value='submit'/>
+        <button class='btn btn-primary' [style.backgroundColor]="isActive ? 'green' : 'blue'" >submit</button>
         <courses></courses>
         <authors></authors>`,
     directives: [CoursesComponent, AuthorsComponent]
@@ -18,5 +19,5 @@ export class AppComponent {
     private title: string = "My first Angualr 2 App";
     private image: string = "https://images.unsplash.com/photo-1465205568425-23fdd3805e49";
     private size: number = 400;
-    private isActive: boolean = false;
+    private isActive: boolean = true;
 }
