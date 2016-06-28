b8e314475baa49f3a411391bf842730690fdf6d4
hrbcksq <hrbcksq@gmail.com>
2016-06-28 19:22:38 +0300

    three various ways of interpolation

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,16 @@ import { AuthorsComponent } from './authors.component'
 @Component({
     selector: 'my-app',
     template: `
-        <h1> My first Angualr 2 App </h1>
+        <h1>{{ title }}</h1>
+        <img src='{{image}}'>
+        <img bind-src='image'>
+        <img [src]='image'>
         <courses></courses>
         <authors></authors>`,
     directives: [CoursesComponent, AuthorsComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    private title: string = "My first Angualr 2 App";
+    private image: string = "https://images.unsplash.com/photo-1465205568425-23fdd3805e49"
+
+ }
