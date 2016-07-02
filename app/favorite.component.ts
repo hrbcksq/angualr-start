f8d5c004a45a9fc64de000b3ac74704c4b89724f
hrbcksq <hrbcksq@gmail.com>
2016-07-02 03:07:07 +0300

    Input properties in decorator, magic-scting problem

--- app/favorite.component.ts
+++ app/favorite.component.ts
@@ -2,7 +2,10 @@ import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'favorite',
-    template: `<i class='glyphicon' [style.font-size]='size' [class.glyphicon-star]='isActive' [class.glyphicon-star-empty]='!isActive' (click)='changeState()'></i>`
+    template: `<i class='glyphicon' [style.font-size]='size' [class.glyphicon-star]='isActive' [class.glyphicon-star-empty]='!isActive' (click)='changeState()'></i>`,
+    inputs: ['isActive:is-favorite'],
+    outputs:[]
+
 })
 export class FavoriteComponent{
     @Input('is-favorite') isActive: boolean = false;
