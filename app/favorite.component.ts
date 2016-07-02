9177488b80f08692b0e5ecaf066b908faf389d96
hrbcksq <hrbcksq@gmail.com>
2016-07-02 04:03:31 +0300

    Styles

--- app/favorite.component.ts
+++ app/favorite.component.ts
@@ -2,8 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'favorite',
-    templateUrl: 'app/favorite.template.html'
-   
+    templateUrl: 'app/favorite.template.html',
+    styleUrls: ['app\\favorite.style.css']   
 })
 export class FavoriteComponent{
     @Input('is-favorite') isActive: boolean = false;
